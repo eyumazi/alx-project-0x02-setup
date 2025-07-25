71dc85fba94d261ef52d57482d0179fd2be2d0bd
Eyuel M. Gedlie <eyuel.mazengia@aastustudent.edu.et>
2025-07-26 02:02:37 +0300

    updated button

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -22,7 +22,9 @@ const Button: React.FC<ButtonProps> = ({
           rounded-full
           text-xs
           transition-colors duration-200 font-semibold
+          ${size === "small" ? "text-xs" : size === "large" ? "text-lg" : "text-sm"}
           ${className}
+          ${shape === "rounded-sm" ? "rounded-sm" : shape === "rounded-full" ? "rounded-full" : "rounded-md"}
         `}
       >
         {label}
